ee2c1ae382b7b9deb11366de93514775faf414b3
cx uname <cx.index@gmail.com>
2012-07-01 18:21:43 +0400

    this way is better

--- q13_max-min-int.go
+++ q13_max-min-int.go
@@ -2,27 +2,27 @@ package main
 
 //import "fmt"
 //and again, we can use slices here
-func maxint(a ...int) int {
+func maxint(a ...int) (tmp int) {
 	tmp := 0
 	for _, v := range a {
 		if v > tmp {
 			tmp = v
 		}
 	}
-	return tmp
+	return
 }
 
-func minint(a ...int) int {
+func minint(a ...int) (tmp int) {
 	tmp := 0
 	for _, v := range a {
 		if v < tmp {
 			tmp = v
 		}
 	}
-	return tmp
+	return
 }
 
 func main() {
-	println(maxint(1, 2, 3, -4, 999, 0 ))
+	println(maxint(1, 2, 3, -4, 999, 0))
 	println(minint(1, 2, 3, -4, -999, 0, -100500))
 }
