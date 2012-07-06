6671afb699289c9aebcc1c51f9670b338791a81e
cx uname <cx.index@gmail.com>
2012-07-06 21:49:22 +0400

    + interfaces

---
+++ q19_mapmod-interfaces.go
@@ -0,0 +1,28 @@
+package main
+//nya nya nya
+//answers says we should do it with a callback, but fuck it
+import "fmt"
+
+func mult(f interface{}) interface{} {
+	fmt.Printf("%T\n", f)
+	switch f.(type) {
+	case int:
+//		f.(int) = 1 //wont work
+	case []int:
+		fmt.Println(f.([]int))
+	case *[]int:
+		fmt.Println(f.(*[]int))
+	case []string:
+		fmt.Println(len(f.([]string)))
+	}
+	return f
+}
+
+func main() {
+	s := []string{"hello", "my", "world"}
+	mult(s)
+	
+	a := []int{1,2,3,43,4}
+	mult(a)
+	mult(&a)
+}
