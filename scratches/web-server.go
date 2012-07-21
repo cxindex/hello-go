f671d762bf0edd44735e2e8e36300bbb75e6380c
cx uname <cx.index@gmail.com>
2012-07-21 22:46:56 +0400

    Update

---
+++ scratches/web-server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	// "strings"
+	"net/http"
+	// "os"
+	"os/exec"
+	// "io/ioutil"
+)
+
+const (
+	Head = `
+<html>
+<head>
+<script type="text/JavaScript">
+<!--
+function timedRefresh(timeoutPeriod) {
+	setTimeout("location.reload(true);",timeoutPeriod);
+}
+//   -->
+</script>
+</head>
+<body onload="JavaScript:timedRefresh(4000);">`
+	Tail = `
+</body>
+</html>`
+)
+
+
+
+func Handler(w http.ResponseWriter, req *http.Request) {
+	out, _ := exec.Command("./sc.sh").Output()
+	fmt.Println(req)
+	fmt.Fprintf(w, "%s %s %s", Head, out, Tail)
+}
+
+func main() {
+	http.HandleFunc("/", Handler)
+	err := http.ListenAndServe(":26666", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
